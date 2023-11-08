20a91e9c94ac7d1353e578e87279d5aa54cebb1c
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-11-08 15:14:32 -0600

    set up phaser scene, config, and game, added basic css

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,12 @@
 // Create a new scene
 
-
+let gameScene = new Phaser.Scene('Game');
 // Set the config of game
+let config = {
+    type: Phaser.AUTO, //phaser will use WebGL (first option) or Canvas (second option)
+    height: 360,
+    scene: gameScene
+};
 
 // create a new game, pass config
+let game = new Phaser.Game(config);
