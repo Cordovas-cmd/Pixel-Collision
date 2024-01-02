305020fab2be768e0fddc30e3ee57ab49555671e
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2024-01-01 18:01:28 -0600

    housekeeping/ deleted all code not having to do with player

--- assets/js/game.js
+++ assets/js/game.js
@@ -52,73 +52,13 @@ gameScene.create = function () {
     // player.flipY = true;
     console.log(player)
 
-
-    // Create enemy sprite
-    // let enemy1 = this.add.sprite(250,180, 'drake')
-
-
-    // declare is this.enemy1 instead of let so we can access them in the game scene.
-    this.enemy1 = this.add.sprite(250,180, 'drake')
-    
-    
-    // scales the enemy just a diff way to do it
-    // enemy1.scaleX = 2;
-    // enemy1.scaleY = 2;
-
-    this.enemy1.scaleX = 2;
-    this.enemy1.scaleY = 2;
-    
-    // set sprite angle methods
-    // enemy1.angle = 45
-    // enemy1.setAngle = 45
-
-
-    // this.enemy1.angle(45);
-    this.enemy1.setAngle(45);
-
-    
-    // use radius for rotation. Rotation based on origin. 
-    
-    // moves the origin and thus the rotation point.
-    // enemy1.setOrigin(0,0);
-
-
-    // Math.PI /4 same as 45 degrees.
-    this.enemy1.rotation = Math.PI
-    this.enemy1.setRotation(Math.PI / 4);
-
-
-    // create second enemy
-    // let enemy2  = this.add.sprite(450,180, 'drake');
-    this.enemy2  = this.add.sprite(450,180, 'drake');
-
-    // another method of resizing a sprite
-    // enemy2.displayWidth = 200;
     console.log(this)
 };
 
 // create update method. up to 60 times per second.
 
 gameScene.update = function(){
-    // moves the sprite along the x axis by 1 every second
-// this.enemy1.x += 1;
 
-this.enemy1.angle += 1;
-if(this.enemy2.y >= 180 && this.enemy2.y <= 300){
-    this.enemy2.y += .5;
-}
-// else if(this.enemy2.y >= 299){
-//     this.enemy2.y += -.5;
-// }
-// this.enemy2.y += -.5;
-console.log(this.enemy2.y)
-
-
-
-// wan't able to toggle direction based on enemy2 y
-// if(this.enemy2.y >= 300){
-//     this.enemy2.y += -1;
-// }
 }
 
 
